42bafcbae993edd996e01653fa92d27845c4494f
Madara Meegama <madarameegama7@gmail.com>
2025-01-01 17:04:50 +0530

    declaring varibales-float,int, bits memory

--- main.go
+++ main.go
@@ -25,6 +25,28 @@ func main(){
 	nameFour := "nayana"
 	fmt.Println(nameFour)
 
+	//ints
+	var ageOne int =25
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne,ageTwo,ageThree)
+
+	//bits and memory
+	var numOne int8 = 125
+	var numTwo int8 = -128
+	var numThree uint8 = 25
+
+	fmt.Println(numOne, numTwo, numThree)
+
+	//float numbers
+	var scoreOne float32 = -1.5
+	var scoreTwo float64 = 8845157846.3
+    scoreThree := 1.5
+
+	fmt.Println(scoreOne, scoreTwo, scoreThree)
+
+
 
 }
 

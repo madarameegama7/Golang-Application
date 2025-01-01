8ecfd29cbe2dc53ac12fe28aaf9c45bd9ef677c1
Madara Meegama <madarameegama7@gmail.com>
2025-01-01 16:54:04 +0530

    Declaring variables-strings

--- main.go
+++ main.go
@@ -4,5 +4,27 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello, Madara")
+
+	//strings
+	//if we declare a variable it must be used otherwise we get an error
+	var nameOne string = "mario"
+	var nameTwo = "luigi"
+	var nameThree string
+
+
+	fmt.Println(nameOne)
+	fmt.Println(nameTwo,nameThree)
+
+	nameOne = "dinu"
+	nameThree = "vidura"
+
+	fmt.Println(nameOne,nameTwo,nameThree)
+
+	//short method to declare variable(we cannot use this outside function)
+
+	nameFour := "nayana"
+	fmt.Println(nameFour)
+
+
 }
 
